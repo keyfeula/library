8960576d27ab365de217b7b53b69bad2aab04393
Key Feula <keyfeula1214@gmail.com>
2024-12-25 18:58:00 -0700

    Work on issue where deleting a book changes the read status of other books

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ function createBookCard(book) {
 
     if (book.readStatus === "Not Read") {
         readBtn.textContent = "Not Read";
-        readBtn.classList.add("not-read");
+        readBtn.classList.toggle("not-read");
     }
     else {
         readBtn.textContent = "Read";
@@ -70,21 +70,6 @@ function createBookCard(book) {
     card.setAttribute("id", myLibrary.length - 1);
 
     cardsContainer.append(card);
-
-    deleteBtn.addEventListener("click", (e) => {
-        let bookIndex = e.target.parentNode.parentNode.getAttribute("id");
-        myLibrary.splice(bookIndex, 1);
-        cardsContainer.textContent = "";
-        for (let book of myLibrary) {
-            createBookCard(book);
-        }
-    });
-
-    readBtn.addEventListener("click", () => {
-        myLibrary[card.getAttribute("id")].toggleReadStatus();
-        readBtn.textContent = myLibrary[card.getAttribute("id")].readStatus;
-        readBtn.classList.toggle("not-read");
-    });
 }
 
 addBookBtn.addEventListener("click", () => {
@@ -99,4 +84,30 @@ submitBtn.addEventListener("click", (e) => {
     let readStatus = readInput.checked ? "Read" : "Not Read";
     let newBook = new Book(titleInput.value, authorInput.value, pagesInput.value, readStatus);
     addBookToLibrary(newBook);
+});
+
+cardsContainer.addEventListener("click", (e) => {
+    const element = e.target;
+    const tagName = element.tagName;
+    if (tagName !== "BUTTON") {
+        return;
+    }
+    const index = element.parentNode.parentNode.getAttribute("id");
+    if (element.classList.contains("read-btn")) {
+        console.log(index);
+        myLibrary[index].toggleReadStatus();       
+        if (element.classList.contains("not-read")) {
+            element.textContent = "Not Read";
+        }
+        else {
+            element.textContent = "Read";
+        }
+    }
+    else if (element.classList.contains("delete-btn")) {
+        myLibrary.splice(index, 1);
+        cardsContainer.textContent = "";
+        for (const book of myLibrary) {
+            createBookCard(book);
+        }
+    }
 });
